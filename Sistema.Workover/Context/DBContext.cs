344aa7442e92469f27af41c017750a3ac2841281
edyfontes <edyfontes@hotmail.com>
2020-12-12 15:11:09 -0300

    Crud

---
+++ Sistema.Workover/Context/DBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Sistema.Workover.Models;
+
+namespace Sistema.Workover.Context
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Produto>Produto { get; set; }
+
+    }
+}
---
+++ Sistema.Workover/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sistema.Workover.Models
+{
+    public class Produto
+    {
+        // key referente a chave primária
+        [Key]
+        public int ID { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public  DateTime UltimaCompra { get; set; }
+        public float Estoque { get; set; }
+
+    }
+}
