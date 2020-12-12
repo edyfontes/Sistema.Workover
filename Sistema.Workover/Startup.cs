e3b2f58ba3ae6a7b4ac39d50b6a088385881b15a
edyfontes <edyfontes@hotmail.com>
2020-12-12 00:16:06 -0300

    Add project files.

---
+++ Sistema.Workover/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sistema.Workover.Startup))]
+namespace Sistema.Workover
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
